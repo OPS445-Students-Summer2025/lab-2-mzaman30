cdc0623fbd4ce9c6d6d2cff9414f81f3995786c4
Md Shahriar Zaman <mzaman30@myseneca.ca>
2025-05-13 09:32:21 -0400

    Completed Lab 2 mzaman30

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Md. Shahriar Zaman
+# Author ID: mzaman30
+# Date Created: 2025/05/13
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Md. Shahriar Zaman
+# Author ID: mzaman30
+# Date Created: 2025/05/13
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
